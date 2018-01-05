4cf74e6088119fb641bec76a10dafce1bd64a0ce
WebSter <wws.dev@gmail.com>
2018-01-05 14:36:40 +0500

    Added rtailLens and example for it

--- task5/task5-1.hs
+++ task5/task5-1.hs
@@ -5,6 +5,8 @@ import Control.Lens
 import Term
 import ReverseList
 
+errorEmptyList = error "Cannot focus on an empty ReverseList"
+
 -- Линза, фокусирующаяся на голове перевернутого списка (для R[1,2,3] - это 3)
 rheadLens :: Lens' (ReverseList a) a
 rheadLens = lens getter setter where
@@ -13,13 +15,27 @@ rheadLens = lens getter setter where
     setter RNil _ = errorEmptyList
     setter (RCons rlst elem) newelem = RCons rlst newelem
 
-errorEmptyList = error "Cannot focus on an empty ReverseList"
-
 -- Примеры для rheadLens
 rhead :: ReverseList a -> a
 rhead = view rheadLens
 
-rlist = fromList [1..5]
-rheadLens_Example1 = rhead rlist -- Выведет голову списка R[1..5], то есть число 5
-rheadLens_Example2 = over rheadLens (*2) rlist -- Выведет список с удвоенной головой, т.е. числом 10 вместо 5
+exampleRList = fromList [1..5]
+rheadLens_Example1 = rhead exampleRList -- Выведет голову списка R[1..5], то есть число 5
+rheadLens_Example2 = over rheadLens (*2) exampleRList -- Выведет список с удвоенной головой, т.е. числом 10 вместо 5
 rheadLens_Example3 = fromList [(x,y) | x <- [1..3], y <- ['a'..'z']] ^. rheadLens . _2 -- Композиция линз (выведет 'z')
+
+-- Линза, фокусирующаяся на хвосте перевернутого списка (для R[1,2,3] - это R[1,2])
+rtailLens :: Lens' (ReverseList a) (ReverseList a)
+rtailLens = lens getter setter where
+    getter RNil = errorEmptyList
+    getter (RCons rlst _) = rlst
+    setter RNil _ = errorEmptyList
+    setter (RCons rlst elem) newrlst = RCons newrlst elem
+
+-- Примеры для rtailLens
+rtail :: ReverseList a -> ReverseList a
+rtail = view rtailLens
+
+rtailLens_Example1 = rtail exampleRList -- Выведет хвост списка R[1..5], то есть R[1,2,3,4]
+rtailLens_Example2 = set rtailLens (fromList [42,43]) exampleRList -- Выведет список R[42,43,5]
+
