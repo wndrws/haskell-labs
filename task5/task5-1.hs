ccd468024a76276886d38da7075b03d094c644f9
WebSter <wws.dev@gmail.com>
2018-01-06 03:20:11 +0500

    Completed task5

--- task5/task5-1.hs
+++ task5/task5-1.hs
@@ -1,8 +1,6 @@
---{-# LANGUAGE RankNTypes #-}
+{-# LANGUAGE RankNTypes #-}
 
 import Control.Lens
-
-import Term
 import ReverseList
 
 errorEmptyList = error "Cannot focus on an empty ReverseList"
@@ -39,3 +37,60 @@ rtail = view rtailLens
 rtailLens_Example1 = rtail exampleRList -- Выведет хвост списка R[1..5], то есть R[1,2,3,4]
 rtailLens_Example2 = set rtailLens (fromList [42,43]) exampleRList -- Выведет список R[42,43,5]
 
+-- Линза, фокусирующаяся на last перевернутого списка (для R[1,2,3] - это 1)
+rlastLens :: Lens' (ReverseList a) a
+rlastLens = lens getter setter where
+    getter RNil = errorEmptyList
+    getter (RCons RNil elem) = elem
+    getter (RCons rlst _) = getter rlst
+    setter RNil _ = errorEmptyList
+    setter (RCons RNil elem) newelem = RCons RNil newelem
+    setter (RCons rlst elem) newelem = RCons (setter rlst newelem) elem
+
+-- Примеры для rlastLens
+rlast :: ReverseList a -> a
+rlast = view rlastLens
+
+rlastLens_Example1 = rlast exampleRList -- Выведет last списка R[1..5], то есть число 1
+rlastLens_Example2 = fmap show exampleRList & rlastLens %~ (++ " - this is the last") -- Укажет на last
+
+-- Линза, фокусирующаяся на init перевернутого списка (для R[1,2,3] - это R[2,3])
+rinitLens :: Lens' (ReverseList a) (ReverseList a)
+rinitLens = lens getter setter where
+    getter RNil = errorEmptyList
+    getter (RCons RNil _) = RNil
+    getter (RCons rlst elem) = RCons (getter rlst) elem
+    setter RNil _ = errorEmptyList
+    setter (RCons RNil _) newrlst = RCons (rtail newrlst) (rhead newrlst)
+    setter (RCons rlst _) newrlst = RCons (RCons RNil (rlast rlst) `rconcat` rtail newrlst) (rhead newrlst)
+
+-- Примеры для rinitLens
+rinit :: ReverseList a -> ReverseList a
+rinit = view rinitLens
+
+rinitLens_Example1 = rinit exampleRList -- Выведет init списка R[1..5], то есть R[2,3,4,5]
+rinitLens_Example2 = exampleRList & rinitLens .~ rtail exampleRList -- Заменит init на tail
+
+-- Линза, фокусирующаяся на элементе перевернутого списка по его индексу (для R[1,2,3] элемент с индексом 0 - это 3)
+rindexLens :: Int -> Lens' (ReverseList a) a
+rindexLens index = if index < 0 then error "Negative index" else lens getter setter where
+    getter RNil = errorEmptyList
+    getter rlist = rget rlist index where 
+        rget :: ReverseList a -> Int -> a
+        rget (RCons _ elem) 0 = elem
+        rget (RCons RNil _) _ = error "Index out of bounds"
+        rget (RCons rlst _) idx = rget rlst $ pred idx
+    setter RNil _ = errorEmptyList
+    setter rlist newelem = rset rlist newelem index where
+        rset :: ReverseList a -> a -> Int -> ReverseList a
+        rset (RCons rlst _) new 0 = RCons rlst new
+        rset (RCons RNil _) _ _ = error "Index out of bounds"
+        rset (RCons rlst elem) new idx = rset rlst new (pred idx) `rconcat` RCons RNil elem
+
+-- Примеры для rindexLens
+rindex :: ReverseList a -> Int -> a
+rindex rlist idx = rlist ^. rindexLens idx
+
+rindexLens_Example1 = exampleRList `rindex` 1 -- Вывдет число 4 из списка R[1..5]
+rindexLens_Example2 = set (rindexLens 1) 100500 exampleRList -- Вернет список R[1,2,3,100500,5]
+rindexLens_Example3 = exampleRList & rindexLens 4 %~ negate -- Вернет список R[-1,2,3,4,5]
