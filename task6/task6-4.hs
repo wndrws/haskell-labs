3536972088e17d5a2da4ab51369b211f8199489f
WebSter <wws.dev@gmail.com>
2018-01-07 20:58:39 +0500

    Fixed bug with parents owning old children

--- task6/task6-4.hs
+++ task6/task6-4.hs
@@ -1,32 +1,32 @@
 import Data.List (sort)
 
 data BinaryTree a = 
-      EmptyBinaryTree
+      EmptyBinaryTree (BinaryTree a)
     | Leaf a (BinaryTree a)
     | Node a (BinaryTree a) (BinaryTree a) (BinaryTree a)
 
 instance (Show a) => Show (BinaryTree a) where
-    show EmptyBinaryTree = "<>"
+    show (EmptyBinaryTree _) = "<>"
     show (Leaf a _) = "L<" ++ show a ++ ">"
     show (Node a left right _) = "(" ++ show left ++ " N<" ++ show a ++ "> " ++ show right ++ ")"
 
 instance (Eq a) => Eq (BinaryTree a) where
-    EmptyBinaryTree == EmptyBinaryTree = True
-    EmptyBinaryTree == _ = False
-    _ == EmptyBinaryTree = False
+    (EmptyBinaryTree _) == (EmptyBinaryTree _) = True
+    (EmptyBinaryTree _) == _ = False
+    _ == (EmptyBinaryTree _) = False
     Leaf a _ == Leaf b _ = a == b
     Node a al ar _ == Node b bl br _ = a == b && al == bl && ar == br
     _ == _ = False
 
 -- Создание пустого дерева
 emptyTree :: (Ord a) => BinaryTree a
-emptyTree = EmptyBinaryTree
+emptyTree = EmptyBinaryTree emptyTree
 
 -- Создание дерева из списка
 -- (позволяет получить сбалансированное дерево ценой предварительной сортировки списка)
 treeFromList :: (Ord a) => [a] -> BinaryTree a
-treeFromList list = treeFromList' list EmptyBinaryTree where
-    treeFromList' [] _ = EmptyBinaryTree
+treeFromList list = treeFromList' list emptyTree where
+    treeFromList' [] _ = emptyTree
     treeFromList' [a] parent = Leaf a parent
     treeFromList' unsortedList parent = 
         let lst = Data.List.sort unsortedList
@@ -39,49 +39,56 @@ treeFromList list = treeFromList' list EmptyBinaryTree where
 
 -- Добавление элемента
 insert :: (Ord a) => BinaryTree a -> a -> BinaryTree a
-insert EmptyBinaryTree val = Leaf val EmptyBinaryTree
+insert (EmptyBinaryTree parent) val = let self = Leaf val (parent `updChild` self) in self
 insert src@(Leaf val parent) newval
-    | newval < val = let self = Node val (Leaf newval self) EmptyBinaryTree parent in self
-    | newval > val = let self = Node val EmptyBinaryTree (Leaf newval self) parent in self
+    | newval < val = let self = Node val (Leaf newval self) emptyTree (parent `updChild` self) in self
+    | newval > val = let self = Node val emptyTree (Leaf newval self) (parent `updChild` self) in self
     | newval == val = src
 insert src@(Node val left right parent) newval
-    | newval < val = let self = Node val (insert left newval) right parent in self
-    | newval > val = let self = Node val left (insert right newval) parent in self
+    | newval < val = let self = Node val (insert left newval) right (parent `updChild` self) in self
+    | newval > val = let self = Node val left (insert right newval) (parent `updChild` self) in self
     | newval == val = src
 
 -- Удаление элемента
 remove :: (Ord a) => BinaryTree a -> a -> BinaryTree a
-remove EmptyBinaryTree _ = EmptyBinaryTree
-remove src@(Leaf val _) valToRemove = if val == valToRemove then EmptyBinaryTree else src
+remove src@(EmptyBinaryTree _) _ = src
+remove src@(Leaf val _) valToRemove = if val == valToRemove then emptyTree else src
 remove (Node val left right parent) valToRemove
-    | valToRemove < val = makeLeafs $ Node val (remove left valToRemove) right parent
-    | valToRemove > val = makeLeafs $ Node val left (remove right valToRemove) parent
-    | valToRemove == val = makeLeafs $ if right == EmptyBinaryTree then left `chParent` parent
+    | valToRemove < val = makeLeafs $ let self = Node val (remove left valToRemove) right (parent `updChild` self) in self
+    | valToRemove > val = makeLeafs $ let self = Node val left (remove right valToRemove) (parent `updChild` self) in self
+    | valToRemove == val = makeLeafs $ if right == emptyTree then left `chParent` parent
         else node where
-            node = Node newval (left `chParent` node) newright parent
+            node = Node newval (left `chParent` node) newright (parent `updChild` node)
             newval = findLeftmostVal right
             newright = remove right newval `chParent` node
 
-chParent :: BinaryTree a -> BinaryTree a -> BinaryTree a
-chParent EmptyBinaryTree _ = EmptyBinaryTree
-chParent (Leaf val _) parent = Leaf val parent
-chParent (Node val left right _) parent = Node val left right parent
+chParent :: (Eq a) => BinaryTree a -> BinaryTree a -> BinaryTree a
+chParent src@(EmptyBinaryTree _) _ = src
+chParent (Leaf val _) parent = let self = Leaf val (parent `updChild` self) in self
+chParent (Node val left right _) parent = let self = Node val left right (parent `updChild` self) in self
+
+updChild :: (Eq a) => BinaryTree a -> BinaryTree a -> BinaryTree a
+updChild (Node val left right parent) child
+    | child == left = let self = Node val child right (parent `updChild` self) in self
+    | child == right = let self = Node val left child (parent `updChild` self) in self
+    | otherwise = error "Wrong child provided"
+updChild tree _ = tree
 
 findLeftmostVal :: (Ord a) => BinaryTree a -> a
 findLeftmostVal (Leaf val _) = val
-findLeftmostVal (Node val EmptyBinaryTree _ _) = val
+findLeftmostVal (Node val (EmptyBinaryTree _) _ _) = val
 findLeftmostVal (Node _ left _ _) = findLeftmostVal left
 
 makeLeafs :: (Ord a) => BinaryTree a -> BinaryTree a
-makeLeafs EmptyBinaryTree = EmptyBinaryTree
+makeLeafs src@(EmptyBinaryTree _) = src
 makeLeafs src@(Leaf _ _) = src
 makeLeafs (Node val left right parent)
-    | left == EmptyBinaryTree && right == EmptyBinaryTree = Leaf val parent
+    | left == emptyTree && right == emptyTree = Leaf val parent
     | otherwise = Node val (makeLeafs left) (makeLeafs right) parent
 
 -- Поиск элемента в дереве
 containsElement :: (Ord a) => BinaryTree a -> a -> Bool
-containsElement EmptyBinaryTree _ = False
+containsElement (EmptyBinaryTree _) _ = False
 containsElement (Leaf val _) target = val == target
 containsElement (Node val left right _) target
     | target < val = containsElement left target
